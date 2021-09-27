9a880c6d4f6e3cd0954342fc52be66e4efd3e884
Mariella CC <MariellaCC@users.noreply.github.com>
2021-09-27 19:15:05 +0200

    query tables preview

--- examples/streamlit/geolocation_prototype/pages/map.py
+++ examples/streamlit/geolocation_prototype/pages/map.py
@@ -12,42 +12,61 @@ def app():
     sql_query_not_null = "SELECT * FROM data WHERE latitude IS NOT NULL or longitude IS NOT NULL"
 
     query_module = kiara.get_operation("table.query.sql")
+    
     query_result = query_module.module.run(table=table_value, query=sql_query_not_null)
-    #query_result = query_module.module.run(table=table_value, query=sql_query_null)
     query_result_value = query_result.get_value_obj("query_result")
     query_result_table = query_result_value.get_value_data()
-
+    query_result_df = query_result_table.to_pandas()
     data = list(query_result_table.to_pandas().to_dict(orient="index").values())
-    
     data_json = json.dumps(data, default=str)
-    
     cleaned_data = json.loads(data_json)
     
+    query_result_null = query_module.module.run(table=table_value, query=sql_query_null)
+    query_result_null_value = query_result_null.get_value_obj("query_result")
+    query_result_null_table = query_result_null_value.get_value_data()
+    query_result_null_df = query_result_null_table.to_pandas()
 
+    
+    map_json = st.session_state.map
+    my_expander = st.sidebar.expander(label='Settings')
 
-    observers = observable(
+    with my_expander:
+        unit = st.selectbox("Projection", ('Mercator', 'Equal Earth', 'Geo Orthographic'))
+
+    map_points = observable(
             "Test",
             notebook="@mariellacc/geolocation",
             targets=["container", "svgLayer", "canvasLayer"],
-            redefine={
-                "data": cleaned_data,
+            redefine={ 
+                "userData": cleaned_data,
+                "mapData": map_json,
+                "chooseProjection": unit,
             },
             observe=["unmapItems"],
         )
 
-    unmapItems = observers.get("unmapItems")
+    unmapItems = map_points.get("unmapItems")
 
-    #print(unmapItems)
+    col1, col2, col3 = st.columns(3)
 
-    if unmapItems is None:
-        st.write("All observations mappable")
+    with col1:
+        mapped_items = st.button(label='Mappable observations')
+        
+    with col2:
+        missing_coords = st.button(label='Missing coordinates')
     
-    if unmapItems is not None:
-        #print(unmapItems)
-        st.write('Unmappable observations')
-        st.dataframe(unmapItems)
+    with col3:
+        unmap_items = st.button(label='Unmappable observations')
 
-
-
-
-
+    
+    if mapped_items:
+        st.dataframe(query_result_df)
+    
+    if missing_coords:
+        st.dataframe(query_result_null_df)
+    
+    if unmap_items:
+        if unmapItems is not None and len(unmapItems) >0:
+            st.dataframe(unmapItems)
+        elif len(unmapItems) == 0:
+            st.write("No unmappable items") 
