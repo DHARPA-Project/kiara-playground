3ecdc3401416bccbc65e5cec96691aebcc5c42bd
Mariella CC <MariellaCC@users.noreply.github.com>
2021-10-28 17:38:53 +0200

    add authors metadata

--- src/kiara_modules/playground/mariella/__init__.py
+++ src/kiara_modules/playground/mariella/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 KIARA_METADATA = {
     "authors": [
+        {"name": "Lorella Viola", "email": "lorella.viola@uni.lu"},
         {"name": "Mariella de Crouy Chanel", "email": "mariella.decrouychanel@uni.lu"}
     ],
 }
